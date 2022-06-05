eed2dc4e8b8ef82708be91b063267f34dbc92dd4
Jin Park <jin225675@naver.com>
2022-06-05 21:46:08 +0900

    add footer

---
+++ src/components/home/Home-Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="footer">
+    여기다가는 footer 내용들을 넣어야겠지
+  </div>
+</template>
+
+<style>
+#footer{
+  display: flex;
+  background-color: black;
+  color: white;
+  align-items: center;
+  justify-content: center;
+  opacity: 0.9;
+  height: 8em;
+}
+</style>
