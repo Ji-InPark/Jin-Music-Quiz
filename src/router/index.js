d3f2c68999953f73ae1cd16d07f2c117ccf25d26
Jin Park <jin225675@naver.com>
2022-06-08 21:04:27 +0900

    build project

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import {createWebHistory, createRouter} from 'vue-router';
+import {createRouter, createWebHashHistory} from 'vue-router';
 
 const routes = [
   {
@@ -15,7 +15,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
