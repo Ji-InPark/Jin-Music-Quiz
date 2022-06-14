948d2c924c70b6a64a0ac94dfc80b362aa51515f
Jin Park <81283634+Ji-InPark@users.noreply.github.com>
2022-06-14 22:56:38 +0900

    Add copyright and github link

--- src/components/home/home-footer.vue
+++ src/components/home/home-footer.vue
@@ -1,17 +1,38 @@
 <template>
-  <div id="footer">
-    여기다가는 footer 내용들을 넣어야겠지
+  <div id="home-footer">
+    <a href="https://github.com/Ji-InPark" id="github-link">Ji-In Park</a>
+    <p id="copyright">Copyright&copy; 2022.Ji-In Park.All rights reserved</p>
   </div>
 </template>
 
-<style>
-#footer{
+<style scoped>
+@font-face {
+  font-family: 'SUIT-Medium';
+  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-Medium.woff2') format('woff2');
+  font-weight: normal;
+  font-style: normal;
+}
+
+#home-footer{
   display: flex;
+  flex-direction: column;
   background-color: black;
   color: white;
   align-items: center;
-  justify-content: center;
+  justify-content: end;
   opacity: 0.9;
-  height: 8em;
+  height: 7.5em;
+  font-family: SUIT-Medium;
+}
+
+#github-link{
+  color: white;
+  font-size: 2em;
+}
+
+#copyright{
+  color: white;
+  font-size: 10px;
+  margin-top: 2em;
 }
 </style>
