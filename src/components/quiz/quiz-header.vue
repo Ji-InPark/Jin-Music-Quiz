a9cc5de53afeb4800b89cb0b4a147c378ceafbef
Jin Park <81283634+Ji-InPark@users.noreply.github.com>
2022-06-18 22:19:45 +0900

    implement youtube audio play

--- src/components/quiz/quiz-header.vue
+++ src/components/quiz/quiz-header.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-
+  <div id="video-container">
+    <transition-group name="refresh-iframe" tag="div">
+      <iframe v-for="(link, index) in list" v-bind:key="link + index"
+      width="0" height="0" v-bind:src="link" title="YouTube video player"
+      allow="autoplay; encrypted-media;" hidden="true"></iframe>
+    </transition-group>
+    <button v-if="isValid" v-on:click="playVideo">test</button>
   </div>
 </template>
 
@@ -15,10 +20,28 @@ export default {
       type: String,
       default: "2010"
     }
+  },
+  data() {
+    return {
+      link: "https://www.youtube.com/embed/lOrU0MH0bMk?start=76&end=79&autoplay=1",
+      list: [],
+      isValid: true
+    }
+  },
+  methods: {
+    playVideo: function (){
+      console.log(this.link)
+      this.list.push(this.link)
+      this.isValid = this.list.length < 5
+    }
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+#video-container{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
 </style>
